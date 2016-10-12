261ac59d7e1d1a1df7ed14ce457daef169f22fc3
croush <claireroush@gmail.com>
2016-10-12 09:46:18 -0500

    Update

--- test.py
+++ test.py
@@ -1,49 +1,7 @@
+import pandas
 import random
 import math
-import pandas
 from sklearn import cross_validation
-
-credit_data = ""        #this is global so I can use it in the choose attribute function?
-train_data = ""
-test_data = ""          #probably should do this a better way
-entropyWhole = ""       #see above. Didn't want to recalculate this every time (slow)
-
-#this is passed the column to calculate entropy on, and the data frame
-#returns the expected entropy. When used, still need to subtract from entropy to get information gain
-
-def pickBest(col, data):
-
-def expectedEntropy(col, data):
-    sum = 0.0
-    e = 0.0
-    labelList = data[col].value_counts().index.tolist() #a list of the labels (A11, A14, etc)
-    numBranches = data[col].value_counts() #number of branches (self explanatory)
-
-    for i in range(len(numBranches)):
-        howManyTrue = len(data.loc[(data[col]==labelList[i]) & (data["Creditability"]==1), [labelList[i],"Creditability"]])
-        #print(howManyTrue)
-        howManyFalse = len(data.loc[(data[col]==labelList[i]) & (data["Creditability"]==2), [labelList[i],"Creditability"]])
-        #print(howManyFalse)
-        branchTotal = howManyTrue+howManyFalse
-        weight = branchTotal/len(data)
-        #e = - len(tempPrior1) / branchTotal * math.log(len(tempPrior1) / branchTotal, 2) - len(tempPrior2) / branchTotal * math.log(len(tempPrior2) / branchTotal, 2)
-        e = - (howManyTrue/branchTotal) * math.log(howManyTrue/branchTotal+.000000001, 2) - (howManyFalse/branchTotal)*math.log(howManyFalse/branchTotal+.000000001,2)
-        sum += e*weight
-    return sum
-
-
-#get the entropy of the data frame
-def entropy(dataSet):
-    sum = 0.0
-
-    priors = []  # to hold the priors (used in entropy comp later)
-    temp = dataSet['Creditability'].value_counts()
-    for i in temp.index:  # for each unique value, calculate the prior
-        priors.append(temp.ix[i]/len(dataSet.index))
-
-    for i in range(len(priors)):
-        sum = sum + (-priors[i] * math.log(priors[i], 2))
-    return sum
 
 
 # the class with functions to be used as entry points when
@@ -79,22 +37,8 @@ class DNode:
     # this should use the training data to determine the best attribute to use
     # as is, it just chooses one at random, but you will fix it to use information gain
     def choose_attribute(self):
-        highestIndex = 0
-        highestValue = 0.0
-        entropyWhole = entropy(test_data)
-        print(len(self.__predictor_columns.columns.values))
-        for i in range(len(self.__predictor_columns.columns.values)):
-            ee = expectedEntropy(self.__predictor_columns.columns.values[i], test_data)
-            if i < 1:
-                highestIndex = i
-                highestValue = entropyWhole - ee
-            elif (entropyWhole - ee) > highestValue:
-                highestIndex = i
-                highestValue = entropyWhole - ee
-
-
-        print("Chose: ", self.__predictor_columns.columns.values[highestIndex])
-        self.__attribute = self.__predictor_columns.columns.values[highestValue]  # what a terrible way to choose the attribute!
+        self.__attribute = random.choice(
+            self.__predictor_columns.columns.values)  # what a terrible way to choose the attribute!
 
     # calling this will continue building the tree from this node given its training examples
     def train(self):
@@ -191,35 +135,3 @@ def accuracy(series1, series2):
         if value == series2.loc[index]:
             correct += 1
     return (correct / len(series1))
-
-credit_data = pandas.read_csv('german.csv')
-
-
-
-train_data, test_data = cross_validation.train_test_split(credit_data,test_size=0.1)
-
-#select only the attributes that are categorical
-categoricalAttribs = list(credit_data.select_dtypes(include=['object']).columns.values)
-
-attributes_to_use = ['Housing', 'Status of existing checking account', 'Credit history']
-
-
-
-my_tree = DTree()
-
-my_tree.fit(train_data[attributes_to_use],train_data['Creditability'])
-
-predictions = my_tree.predict(test_data[attributes_to_use])
-
-
-entropy = entropy(credit_data)
-#ee1 = expectedEntropy('Duration in month',credit_data)
-#print(entropy - ee1)
-#ee2 = expectedEntropy('Credit history', credit_data)
-print(accuracy(predictions, test_data['Creditability']))
-#my_tree.print_tree()
-
-
-
-#only use a few columns and test, then move to all categorical
-#accuracy on training data should be 100% or close
